83da0fcae3785de9167cb96ea9ad5170661ca3bc
WaterFace <kevinvisserweckworth@gmail.com>
2022-09-17 04:20:11 -0400

    started implementing other hooks needed

--- include/Hooks.h
+++ include/Hooks.h
@@ -12,10 +12,32 @@ namespace DetachedLightning {
     static inline REL::Relocation<decltype(m_beamProjectileConstructor)> m_originalBeamProjectileConstructor;
   };
 
-  private:
-
-
+  class TESObjectREFR_SetPositionHook {
   public:
+    static void Hook(SKSE::Trampoline& trampoline);
+  private:
+    static void m_SetPosition(RE::BeamProjectile* proj, RE::NiPoint3* pos);
+    static REL::Relocation<decltype(m_SetPosition)>& m_getSetPosition();
 
+    static inline REL::Relocation<decltype(m_SetPosition)> m_originalUpdatePos;
   };
+  class TESObjectREFR_SetRotationXHook {
+  public:
+    static void Hook(SKSE::Trampoline& trampoline);
+  private:
+    static void m_SetRotationX(RE::BeamProjectile* proj, RE::NiPoint3* pos);
+    static REL::Relocation<decltype(m_SetRotationX)>& m_getSetRotationX();
+
+    static inline REL::Relocation<decltype(m_SetRotationX)> m_originalUpdatePos;
+  };
+  class TESObjectREFR_SetRotationZHook {
+  public:
+    static void Hook(SKSE::Trampoline& trampoline);
+  private:
+    static void m_SetRotationZ(RE::BeamProjectile* proj, RE::NiPoint3* pos);
+    static REL::Relocation<decltype(m_SetRotationZ)>& m_getSetRotationZ();
+
+    static inline REL::Relocation<decltype(m_SetRotationZ)> m_originalUpdatePos;
+  };
+
 }
