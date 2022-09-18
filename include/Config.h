d58e70bafbb2b120056e9b5571ddad24c4ee24ef
WaterFace <kevinvisserweckworth@gmail.com>
2022-09-18 02:25:40 -0400

    Logging level depends on debug/release

--- include/Config.h
+++ include/Config.h
@@ -1,22 +1,30 @@
 #include <SKSE/SKSE.h>
 
 namespace DetachedLightning {
-    class Debug {
-    public:
-        spdlog::level::level_enum GetLogLevel() const { return m_logLevel; }
-        spdlog::level::level_enum GetFlushLevel() const { return m_flushLevel; }
+  class Debug {
+  public:
+    spdlog::level::level_enum GetLogLevel() const { return m_logLevel; }
+    spdlog::level::level_enum GetFlushLevel() const { return m_flushLevel; }
 
-    private:
-        spdlog::level::level_enum m_logLevel{spdlog::level::level_enum::debug};
-        spdlog::level::level_enum m_flushLevel{spdlog::level::level_enum::trace};
+  private:
+    spdlog::level::level_enum m_logLevel{
+#ifdef NDEBUG
+          spdlog::level::level_enum::warn
+#else
+          spdlog::level::level_enum::debug
+#endif
     };
-
-    class Config {
-    public:
-        const Debug& GetDebug() const { return m_debug; };
-        static const Config& GetSingleton();
-
-    private:
-        Debug m_debug;
+    spdlog::level::level_enum m_flushLevel{
+      spdlog::level::level_enum::trace
     };
+  };
+
+  class Config {
+  public:
+    const Debug& GetDebug() const { return m_debug; };
+    static const Config& GetSingleton();
+
+  private:
+    Debug m_debug;
+  };
 }  // namespace DetachedLightning
